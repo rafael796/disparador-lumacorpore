a12165f01b2c9ff4ceda34a750bea87c6ae7ed8c
unknown <rafa796@gmail.com>
2026-05-07 12:20:06 -0300

    Add authentication

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ require('dotenv').config();
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Upload config
